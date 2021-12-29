62e2ca97e92d5a4165f9066256a7940d3d20221e
David Rubinstein <drubinstein91@gmail.com>
2021-12-28 22:24:31 -0500

    Add yarn,node,java,python version managers

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# TODO: This is linux specific. Should write one that sets up brew and such at some point.
+
 function update(){
     git submodule update
 }
@@ -15,21 +17,22 @@ function setup(){
     CONFIG_ROOT="$( cd "$(dirname "$0")" ; pwd -P )"
 
     # check if ln is GNU or BSD
-    if ln --version 2>/dev/null | grep -q 'oreutils'; then
+#    if ln --version 2>/dev/null | grep -q 'oreutils'; then
         # GNU
         echo "GNU detected"
         for conf in ".gitconfig" ".vim" ".tmux.conf" ".zshrc"; do
-            ln -sf ~/${conf} ${CONFIG_ROOT}/${conf}
+            #ln -sf ~/${conf} ${CONFIG_ROOT}/${conf}
+            ln -sf ${CONFIG_ROOT}/${conf} ~/${conf} 
         done
-        ln -sf ~/.vim/basic.vim ${CONFIG_ROOT}/vimrc/vimrcs/basic.vim
-    else
+        ln -sf  ${CONFIG_ROOT}/vimrc/vimrcs/basic.vim ~/.vim/basic.vim
+#    else
         # BSD
-        echo "BSD Detected"
-        for conf in ".gitconfig" ".vim" ".tmux.conf" ".zshrc"; do
-            ln -sf ${CONFIG_ROOT}/${conf} ~/${conf}
-        done
-        ln -sf ${CONFIG_ROOT}/vimrc/vimrcs/basic.vim ~/.vim/basic.vim
-    fi
+#        echo "BSD Detected"
+#        for conf in ".gitconfig" ".vim" ".tmux.conf" ".zshrc"; do
+#            ln -sf ${CONFIG_ROOT}/${conf} ~/${conf}
+#        done
+#        ln -sf ${CONFIG_ROOT}/vimrc/vimrcs/basic.vim ~/.vim/basic.vim
+#    fi
 
     # Setup vim
     echo "Installing vim plugins"
@@ -45,6 +48,31 @@ function setup(){
     cd ..
     rm -rf fonts
     echo "To use oh-my-zsh with powerline set your terminal's font to a powerline font"
+
+    # TODO: Consider putting the download-install latest bits into some function
+    # Alternatively, maybe I should just learn nix
+
+    # install Node version manager and install lts release
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+    source ~/.zshrc
+    nvm install --lts
+    nvm use --lts
+
+    # Install Yarn version manager and its most recent version
+    curl -s https://raw.githubusercontent.com/tophat/yvm/master/scripts/install.js | node
+    source ~/.zshrc
+    yvm install latest
+    yvm use latest
+
+    # Install Jenv (Java version manager) 
+    git clone https://github.com/jenv/jenv.git ~/.jenv
+    source ~/.zshrc
+
+    # Install Pyenv (python version manager) and latest python
+    curl https://pyenv.run | bash
+    source ~/.zshrc
+    latest_python_version=$(curl https://www.python.org/downloads/ | grep 'Download Python' | awk '{$1=$1;print}' | cut -d " " -f 5 | sed -r 's/(<\/a>)+$//' | grep -v '^$' | head -n 1)
+    pyenv install ${latest_python_version}
 }
 
 $1
